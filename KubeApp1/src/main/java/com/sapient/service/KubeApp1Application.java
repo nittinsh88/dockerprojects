0ba079d446095fa0ace1d72e4321e16055166fa3
nittinsh88 <nittinsh88@gmail.com>
2018-08-21 17:31:01 +0530

    kubernetes pods,routing(ingress),services and two application of spring boot

---
+++ KubeApp1/src/main/java/com/sapient/service/KubeApp1Application.java
@@ -0,0 +1,12 @@
+package com.sapient.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubeApp1Application.class, args);
+	}
+}
---
+++ KubeApp2/src/main/java/com/sapient/service/KubeApp2Application.java
@@ -0,0 +1,12 @@
+package com.sapient.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubeApp2Application.class, args);
+	}
+}
